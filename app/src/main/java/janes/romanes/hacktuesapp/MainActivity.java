2ada7bae1e6fcf46ee9a4c63cc882cf03956de85
Nick <64863561+NickProgrammerGaming@users.noreply.github.com>
2023-03-08 21:37:29 +0200

    Initial Commit

---
+++ app/src/main/java/janes/romanes/hacktuesapp/MainActivity.java
@@ -0,0 +1,14 @@
+package janes.romanes.hacktuesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
