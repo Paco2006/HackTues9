22313de766af547f907afd9fb0756ae5fe231cca
Nick <64863561+NickProgrammerGaming@users.noreply.github.com>
2023-03-09 03:15:49 +0200

    About us + Why use our app

---
+++ app/src/main/java/janes/romanes/hacktuesapp/WhyUseOurApp.java
@@ -0,0 +1,14 @@
+package janes.romanes.hacktuesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WhyUseOurApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_why_use_our_app);
+    }
+}
