7f9e796858054f1645ecdd07229c498be65c8677
WaifuPower <pavleti06@gmail.com>
2023-03-11 02:18:11 +0200

    menu+main activity+sub+navigation+firebase

--- app/src/main/java/janes/romanes/hacktuesapp/MainActivity.java
+++ app/src/main/java/janes/romanes/hacktuesapp/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 drawerLayout.openDrawer(GravityCompat.START);
+
             }
         });
 
