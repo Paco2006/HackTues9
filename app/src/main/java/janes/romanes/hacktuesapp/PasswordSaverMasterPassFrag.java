f22d93b7ab5406692a2b0734101d4a2c53064806
WaifuPower <pavleti06@gmail.com>
2023-03-11 02:06:10 +0200

    menu+main activity+sub+navigation+firebase

--- app/src/main/java/janes/romanes/hacktuesapp/PasswordSaverMasterPassFrag.java
+++ app/src/main/java/janes/romanes/hacktuesapp/PasswordSaverMasterPassFrag.java
@@ -1,14 +1,9 @@
 package janes.romanes.hacktuesapp;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,37 +11,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import janes.romanes.hacktuesapp.SavedPasswordsFrag;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PasswordSaverMasterPassFrag#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PasswordSaverMasterPassFrag extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     public PasswordSaverMasterPassFrag() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PasswordSaverMasterPassFrag.
-     */
-    // TODO: Rename and change types and number of parameters
     public static PasswordSaverMasterPassFrag newInstance(String param1, String param2) {
         PasswordSaverMasterPassFrag fragment = new PasswordSaverMasterPassFrag();
         Bundle args = new Bundle();
@@ -56,8 +32,6 @@ public class PasswordSaverMasterPassFrag extends Fragment {
         return fragment;
     }
 
-
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,8 +39,6 @@ public class PasswordSaverMasterPassFrag extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-
     }
 
     private Button btnEnter, btnChangePass;
@@ -74,20 +46,16 @@ public class PasswordSaverMasterPassFrag extends Fragment {
 
     public static String masterPass = "1234";
 
-
     Activity activity;
     View parentHolder;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         activity = getActivity();
         parentHolder = inflater.inflate(R.layout.fragment_password_saver_master_pass, container, false);
-
         btnEnter = parentHolder.findViewById(R.id.btnEnter);
         btnChangePass = parentHolder.findViewById(R.id.btnChangePass);
         passwordInput = parentHolder.findViewById(R.id.passwordInput);
-
 
         btnEnter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -115,12 +83,11 @@ public class PasswordSaverMasterPassFrag extends Fragment {
         else
         {
             Toast.makeText(getContext(), "Wrong Password!", Toast.LENGTH_SHORT).show();
-
         }
     }
 
     private void changeMasterPassword()
     {
-        // Go to another activity and change the master pass
+
     }
 }
--- app/src/main/java/janes/romanes/hacktuesapp/SavedPasswordsFrag.java
+++ app/src/main/java/janes/romanes/hacktuesapp/SavedPasswordsFrag.java
@@ -5,17 +5,12 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -24,35 +19,18 @@ import java.util.ArrayList;
 
 import janes.romanes.hacktuesapp.PasswordGeneratorFrag;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SavedPasswordsFrag#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SavedPasswordsFrag extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     public SavedPasswordsFrag() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SavedPasswordsFrag.
-     */
-    // TODO: Rename and change types and number of parameters
     public static SavedPasswordsFrag newInstance(String param1, String param2) {
         SavedPasswordsFrag fragment = new SavedPasswordsFrag();
         Bundle args = new Bundle();
@@ -73,23 +51,17 @@ public class SavedPasswordsFrag extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-
     }
 
     Activity activity;
     View parentHolder;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         activity = getActivity();
         parentHolder = inflater.inflate(R.layout.fragment_saved_passwords, container, false);
-
         addPasswordBtn = parentHolder.findViewById(R.id.addPasswordBtn);
         listView = parentHolder.findViewById(R.id.listView);
-
         UsersAdapter usersAdapter = new UsersAdapter(
                 this.getContext(),
                 users
@@ -118,8 +90,7 @@ public class SavedPasswordsFrag extends Fragment {
     }
 
     private void addPassword()
-    {
-        Navigation.findNavController(getView()).navigate(R.id.toPassGen);
+    {Navigation.findNavController(getView()).navigate(R.id.toPassGen);
     }
 
     private void copyPasswordToClipboard(String password)
